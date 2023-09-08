4221e4aba6a722d5390ffa6955f7e0cbb8dfacc2
philip-git-07 <141561502+philip-git-07@users.noreply.github.com>
2023-09-08 22:18:40 +0530

    Add files via upload

---
+++ DockerspringbootApplication/src/test/java/com/staragile/DockerspringbootApplication/DockerspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.staragile.DockerspringbootApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
